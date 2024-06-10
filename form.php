66b074420ee5947f6c771e4535b510bb52fc034c
Mftr96 <matteo.federici96@gmail.com>
2024-06-10 15:27:30 +0200

    first commit

---
+++ form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+    <h1>form molto brutto</h1>
+
+    <form action="print.php" method="GET">
+        <input type="text" placeholder="inserisci qui il tuo nome " name="nome">
+        <textarea name="text" placeholder="scrivi qui " id=""></textarea>
+        <button type="submit">invia testo</button>
+
+    </form>
+    
+</body>
+</html>
---
+++ print.php
@@ -0,0 +1,24 @@
+<?php
+$text=$_GET["text"];
+$nome=$_GET["nome"]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>testo inviato </title>
+</head>
+<body>
+    <p>il testo inviato è lungo <?php
+    echo strlen($text)
+    
+    ?>
+    caratteri
+    </p>
+
+    <p><?php echo $nome ?></p>
+    
+</body>
+</html>
